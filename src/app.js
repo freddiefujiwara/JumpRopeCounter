efccc7435dcbe9feb75623dfb84b2c7d76ed58c8
Freddie FUJIWARA <github@ze.gs>
2015-08-14 05:18:18 +0900

    Jumping algorithm first version

--- src/app.js
+++ src/app.js
@@ -6,55 +6,61 @@
 
 var UI = require('ui');
 var Vector2 = require('vector2');
+var Accel = require('ui/accel');
 
+/**
+ * Configuration Start
+ */
+Accel.config.rate   = 10;
+Accel.config.sample = 1;
+var THRESHOLD       = 500;
+/**
+ * Configuration end
+ */
 var main = new UI.Card({
   title: 'Jump Rope Counter',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+  body:  'PUSH BUTTON =>'
 });
-
 main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
+main.on('click', 'select', function() {
+ 
+  Accel.init();
+  var in_the_air = false;
+  var count      = 0;
+  var position_x = 0;
+  Accel.peek(function(e) {
+    position_x = e.accel.x;
   });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
+  Accel.on('data', function(e) {
+    var diff_x = position_x - e.accel.x;
+    if(THRESHOLD < diff_x && !in_the_air){
+      in_the_air = true;
+    }
+    if((0 - THRESHOLD) > diff_x && in_the_air){
+      count ++;
+      in_the_air = false;
+      textfield.text(count);
+    }
+    position_x = e.accel.x;
+    console.log('X='+ diff_x +'    in_the_air='+in_the_air);
   });
-  menu.show();
-});
-
-main.on('click', 'select', function(e) {
+/**
+ *  Counter UI start
+ */
   var wind = new UI.Window({
     fullscreen: true,
   });
   var textfield = new UI.Text({
     position: new Vector2(0, 65),
     size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
+    font: 'bitham-42-bold',
+    text: count,
     textAlign: 'center'
   });
   wind.add(textfield);
   wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+/**
+ *  Counter UI end
+ */
 });
